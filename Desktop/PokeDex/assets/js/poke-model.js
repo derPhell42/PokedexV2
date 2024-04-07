e4dc092395afdc727c2ced9ebcb3ffd172e3c2ba
Ph <theotherph@hotmail.com>
2024-04-07 19:57:45 -0300

    Limpando redundancias

---
+++ Desktop/PokeDex/assets/js/poke-model.js
@@ -0,0 +1,33 @@
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+    photoSh;
+    photo3DMale;
+    photo3DMaleSh
+    photo3DFemale;
+    photo3DFemale;
+    photoShowdownM;
+    photoShowdownMSH;
+    photoShowdownF;
+    photoShowdownFSH;
+    photoBWGif;
+    photoBWGifF;
+    photoBWGifFSH;
+    photoBWGifFFSH;
+    hp;
+    atk;
+    def;
+    spatak;
+    spdef;
+    speed;
+    height;
+    weight;
+    ablt1;
+    ablt2;
+    ablt3hidden;
+    icon;
+    cries;
+}
